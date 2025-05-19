aa107b2aa370e22ce0457fdb5264727d217ee08b
AlejandroDavidSilva <alejandrodavidsilva62@gmail.com>
2025-05-18 21:03:19 -0300

    Organizacion en bloques, creacion de predicados libreo_prestado y siguiendo_curso [David]

--- PMI-LOGICA.pl
+++ PMI-LOGICA.pl
@@ -4,6 +4,7 @@
 %biblioteca_libros_ocupados[ ] %bilbioteca tiene toodos los libros y lleva el registro de libros prestados.
 
 
+%-------------------------------LIBROS------------------------------------------------------------
 libro(l1).
 libro(l2).
 libro(l3).
@@ -14,19 +15,24 @@ libro(l7).
 libro(l8).
 libro(l9).
 libro(l10).
+%todos los libros creados, son los recomenda la profesora
 
 %listaLibros_sugeridos_profe[l1,l2,l3,l4,l5,l6,l7,l8,l9,l10].
+%-------------------------------------------------------------------------------------------
 
+%----------------------------ALUMNOS---------------------------------------------------------------
 alumno(a1).
 alumno(a2).
 alumno(a3).
 alumno(a4).
 alumno(a5).
+%todos los alumnos estan matriculados
+%-------------------------------------------------------------------------------------------
 
 
 %listaAlumnos(a1,a2,a3,a4,a5).
 
-
+%---------------------------------PREFERENCIAS------------------------------------------
 preferencia(a1,l1).
 preferencia(a1,l5).
 preferencia(a1,l7).
@@ -39,28 +45,37 @@ preferencia(a3,l1).
 preferencia(a3,l2).
 preferencia(a3,l3).
 
+%-------------------------------------------------------------------------------------------
+
 
 
 %QUE PASA SI ALUMNO SOLO ME DA 2 O 4 O MAS DE 4 RECOMENDACIONES DE LIBRO, DEBO REALIZAR OTRO PREDICADO?
 
+%------------------------------------LIBRO ASIGNADO-------------------------------------------------------
 libro_asignado(a1,l1).
 libro_asignado(a2,l7).
 libro_asignado(a3,l3).
 libro_asignado(a2,l8).
 
+%-------------------------------------------------------------------------------------------
+
+%------------------------------------LIBRO PRESTADO-------------------------------------------------------
 prestado(l1).
 prestado(l2).
 prestado(l3).
 prestado(l8).
+%-------------------------------------------------------------------------------------------
 
-%asignacion(a1,l1,l2)
-%asignacion(a2,l3,l4)
-%asignacion(a3,l5,l6)
-%asignacion(a4,l7,l8)
-%asignacion(a5,l9,l10)
 
+%-------------------------------------REGLAS------------------------------------------------------
 preferencia_alumno(ALUMNO,LIBRO) :- preferencia(ALUMNO,LIBRO).
 
 libro_prestado(LIBRO) :- prestado(LIBRO).
 
+siguiendo_curso(X) :- preferencia(X, _).                   %COMO EVITAR QUE ITERE MAS DE 1 VEZ CON EL MISMO ALUMNO
 
+
+                
+
+
+%-------------------------------------------------------------------------------------------
