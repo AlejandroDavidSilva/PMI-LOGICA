a810ce1b438a14e28af14f6e1f588b1a6fe532aa
Silva Alejandro David <alejandrosilvadavid.13@gmail.com>
2025-05-26 10:59:03 -0300

    Refacto Correccion de profesora

--- PMI-LOGICA.pl
+++ PMI-LOGICA.pl
@@ -5,33 +5,33 @@
 
 
 %-------------------------------LIBROS------------------------------------------------------------
-libro(l1).
-libro(l2).
-libro(l3).
-libro(l4).
-libro(l5).
-libro(l6).
-libro(l7).
-libro(l8).
-libro(l9).
-libro(l10).
+%libro(l1).
+%libro(l2).
+%libro(l3).
+%libro(l4).
+%libro(l5).
+%libro(l6).
+%libro(l7).
+%libro(l8).
+%libro(l9).
+%libro(l10).
 
 %todos los libros creados, son los recomenda la profesora
 
-%listaLibros_sugeridos_profe[l1,l2,l3,l4,l5,l6,l7,l8,l9,l10].
+listaLibros([l1,l2,l3,l4,l5,l6,l7,l8,l9,l10]).
 %-------------------------------------------------------------------------------------------
 
 %----------------------------ALUMNOS---------------------------------------------------------------
-alumno(a1).
-alumno(a2).
-alumno(a3).
-alumno(a4).
-alumno(a5).
+%alumno(a1).
+%alumno(a2).
+%alumno(a3).
+%alumno(a4).
+%alumno(a5).
 %todos los alumnos estan matriculados
 %-------------------------------------------------------------------------------------------
 
 
-%alumnos(a1,a2,a3,a4,a5).
+alumnos([a1,a2,a3,a4,a5]).
 
 %---------------------------------PREFERENCIAS------------------------------------------
 preferencia(a1,l1).
@@ -77,9 +77,10 @@ siguiendo_curso(X) :- preferencia(X, _).
 
 asignacionLibro_valida(Alumno, Libro) :-
     alumno(Alumno),
+    libro(Libro),
     preferencia(Alumno, Libro),  % aca verifica que haya dado preferencia, x lo tanto, sigue cursando
-    not(prestado(Libro)),
-    libro(Libro).
+    not(prestado(Libro)).
+
 
 
 no_asignado(_,[]).
@@ -87,15 +88,44 @@ no_asignado(Libro, [LibroAsignado|RestoLibros]):-
                    Libro \= LibroAsignado,
                    no_asignado(Libro, RestoLibros).
 
-agregar_libro(Libro, LibrosAsignados, [Libro|LibrosAsignados]):- true.
+agregar_libro(Libro, LibrosAsignados, [Libro|LibrosAsignados]).
+
+
+generar_asignaciones([], [], _).  % caso base: sin alumnos, sin asignaciones, caso en el que corta la iteracion recursiva
+generar_asignaciones([Alumno | RestoAlumnos], [(Alumno, Libro) | RestoAsignaciones], ListaLibros):-
+    preferencia(Alumno, Libro),
+    member(Libro,ListaLibros),
+    not(prestado(Libro)),
+    quitarLibro(Libro, ListaLibros, ListaLibrosMODIF),
+    generar_asignaciones(RestoAlumnos, RestoAsignaciones, ListaLibrosMODIF).
+generar_asignaciones([Alumno | RestoAlumnos], RestoAsignaciones, ListaLibros):-
+     generar_asignaciones(RestoAlumnos, RestoAsignaciones, ListaLibros).
+    
+    
+
+
+    armarLista(listaLibros_sugeridos_profe)
+    asignacionLibro_valida(Alumno, Libro),
+    no_asignado(Libro, LibrosAsignados);
+    not(agregar_libro(Libro, LibrosAsignados, LibrosAsignados2)),
+    generar_asignaciones(RestoAlumnos, RestoAsignaciones, LibrosAsignados2).
 
 
 generar_asignaciones([], _, _).  % caso base: sin alumnos, sin asignaciones, caso en el que corta la iteracion recursiva
-generar_asignaciones([Alumno | RestoAlumnos], [(Alumno, Libro) | RestoAsignaciones], LibrosAsignados):-
+generar_asignaciones([Alumno | RestoAlumnos], [(Alumno, Libro) | RestoAsignaciones], [Libro, RestoLibros]):-
+                             asignacionLibro_valida(Alumno, Libro),
 
-    asignacionLibro_valida(Alumno, Libro),
-    no_asignado(Libro, LibrosAsignados); agregar_libro(Libro, LibrosAsignados, LibrosAsignados),
-    generar_asignaciones(RestoAlumnos, RestoAsignaciones, LibrosAsignados).
+
+
+solucion(Alumnos , P , M ):-
+                 alumnos(A),
+                 listaLibros(L),
+                 generar_asignaciones(A ,Alumnos, L ),
+                 calcular_Porcentaje(alumnos(A), P),
+                 calcular_Media(alumnos(A), M).
+
+
+
 
     
 %-------------------------------------------------------------------------------------------
